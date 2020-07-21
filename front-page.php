4488208e16c90918e8840b3240763d5d4b3166f1
ngangavic <ngangavictor10@gmail.com>
2020-07-21 12:48:30 +0300

    enqueue stylesheet

--- front-page.php
+++ front-page.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="col">
+            left
+        </div>
+
+        <div class="col">
+            right
+        </div>
+
+    </div>
+</div>
 
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
+<?php
+
+function load_stylesheets(){
+    wp_register_style('bootstrap',get_template_directory_uri().'/css/bootstrap.min.css');
+    $dependencies = array('bootstrap');
+    wp_enqueue_style('stylesheet');
+    wp_enqueue_style( 'wordpress-theme-style', get_stylesheet_uri(), $dependencies );
+}
+
+
+//add_action('wp_enqueue_scripts','load_stylesheets');
+add_action( 'wp_enqueue_scripts', 'load_stylesheets' );
+//add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_scripts' );
 
