6a20d64713342a929db9d4000d72172c9abbea69
ngangavic <ngangavictor10@gmail.com>
2020-07-21 14:26:33 +0300

    blog post

--- archive.php
+++ archive.php
@@ -1,1 +1,22 @@
-<?php
+<?php get_header(); ?>
+
+<div class="container-fluid">
+    <h1>
+        <?php single_cat_title(); ?>
+    </h1>
+
+    <?php
+    if(have_posts()):while(have_posts()):the_post();
+        ?>
+        <h3>
+            <?php the_title(); ?>
+        </h3>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>"> Read more...</a>
+
+    <?php endwhile; endif ?>
+
+</div>
+
+
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,1 +1,19 @@
-<?php
+<?php get_header(); ?>
+
+<div class="container-fluid">
+    <h5>
+        <?php the_title(); ?>
+    </h5>
+
+    <?php
+    if(have_posts()):while(have_posts()):the_post();
+        ?>
+
+        <?php the_content(); ?>
+
+    <?php endwhile; endif ?>
+
+</div>
+
+
+<?php get_footer(); ?>
